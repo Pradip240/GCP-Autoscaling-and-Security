a5ff5b37953653b9d6298cf8577c67b19d5ca298
Pradip Solanki <73014474+Pradip240@users.noreply.github.com>
2026-03-27 19:47:53 +0530

    Delete node_exporter.sh

--- node_exporter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-VERSION="1.8.1"
-cd /opt
-
-wget -q https://github.com/prometheus/node_exporter/releases/download/v${VERSION}/node_exporter-${VERSION}.linux-amd64.tar.gz
-tar xzf node_exporter-${VERSION}.linux-amd64.tar.gz
-mv node_exporter-${VERSION}.linux-amd64 node_exporter
